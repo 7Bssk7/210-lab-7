2f02721cc4bc002b2bbc210003fbceff247b722b
7Bssk7 <arhip.fin3@gmail.com>
2025-09-14 16:48:43 -0700

    Update

--- 210-lab-7.cpp
+++ 210-lab-7.cpp
@@ -1,9 +1,26 @@
 // COMSC 210 | Lab 7 | Arkhip Finski
 // IDE used: Visual Studio
 #include <iostream>
+#include <string>
 using namespace std;
 
+const int SIZE = 5;
+
+string* reverseArray();
+void displayArray(string *arr);
+
 int main(){
+    string *listArr = nullptr;
+    listArr = new string[SIZE]{"Janet", "Jeffe", "Jin", "Joe", "Junio"};
+    cout << "Original array: ";
+    displayArray(listArr); 
 
     return 0;
+}
+
+void displayArray(string *arr){
+    for(int i = 0; i < SIZE; ++i){
+        cout << *(arr + i) << " ";
+    }
+    cout << "\n";
 }
